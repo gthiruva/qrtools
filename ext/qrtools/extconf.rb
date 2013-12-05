5bacdf01a6238eb10b975abc76cbf1a6b7ff8aea
George Thiruvathukal <george@thiruva.com>
2013-12-04 21:28:23 -0800

    Trying to clean up extconf.rb to better grab include/lib configs via pkg-config

--- ext/qrtools/extconf.rb
+++ ext/qrtools/extconf.rb
@@ -5,23 +5,23 @@ require 'mkmf'
 LIBDIR = RbConfig::CONFIG['libdir']
 INCLUDEDIR = RbConfig::CONFIG['includedir']
 
-HEADER_DIRS = [
-    '/opt/local/include/opencv',
-]
+HEADER_DIRS = [LIBDIR]
+#    '/opt/local/include/opencv',
+#]
 
 HEADER_DIRS += `pkg-config --cflags opencv`.split.collect { |i| if i.start_with?("-I") then i[2..-1] else i end }
 HEADER_DIRS += `pkg-config --cflags libqrencode`.split.collect { |i| if i.start_with?("-I") then i[2..-1] else i end }
 
-LIB_DIRS = [
-  LIBDIR,
-  '/opt/local/lib',
-]
+LIB_DIRS = [INCLUDEDIR]
+#  LIBDIR,
+#  '/opt/local/lib',
+#]
 
 LIB_DIRS += `pkg-config --libs opencv`.split.collect { |i| if i.start_with?("-L") then i[2..-1] else i end }
 LIB_DIRS += `pkg-config --libs libqrencode`.split.collect { |i| if i.start_with?("-L") then i[2..-1] else i end }
 
-dir_config('opencv', "/opt/local/include/opencv", "/opt/local/lib")
-dir_config('qrencode', "/opt/local/include", "/opt/local/lib")
+#dir_config('opencv', "/opt/local/include/opencv", "/opt/local/lib")
+#dir_config('qrencode', "/opt/local/include", "/opt/local/lib")
 %w{ qrencode opencv_core opencv_highgui }.each do |lib|
   abort "need #{lib}" unless have_library(lib)
 end
