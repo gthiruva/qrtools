34608306a03e444929a0815bafc0fe8c0cee782d
George Thiruvathukal <george@thiruva.com>
2013-12-04 23:44:12 -0800

    More tinkering with dependent header/lib directories

--- ext/qrtools/extconf.rb
+++ ext/qrtools/extconf.rb
@@ -9,19 +9,19 @@ HEADER_DIRS = [INCLUDEDIR]
 #    '/opt/local/include/opencv',
 #]
 
-HEADER_DIRS += `pkg-config --cflags opencv`.split.collect { |i| if i.start_with?("-I") then i[2..-1] else i end }
-HEADER_DIRS += `pkg-config --cflags libqrencode`.split.collect { |i| if i.start_with?("-I") then i[2..-1] else i end }
+HEADER_DIRS += `pkg-config --cflags-only-I opencv`.split.collect { |i| if i.start_with?("-I") then i[2..-1] else i end }
+HEADER_DIRS += `pkg-config --cflags-only-I libqrencode`.split.collect { |i| if i.start_with?("-I") then i[2..-1] else i end }
 
 LIB_DIRS = [LIBDIR]
 #  LIBDIR,
 #  '/opt/local/lib',
 #]
 
-LIB_DIRS += `pkg-config --libs opencv`.split.collect { |i| if i.start_with?("-L") then i[2..-1] else i end }
-LIB_DIRS += `pkg-config --libs libqrencode`.split.collect { |i| if i.start_with?("-L") then i[2..-1] else i end }
+LIB_DIRS += `pkg-config --libs-only-L opencv`.split.collect { |i| if i.start_with?("-L") then i[2..-1] else i end }
+LIB_DIRS += `pkg-config --libs-only-L libqrencode`.split.collect { |i| if i.start_with?("-L") then i[2..-1] else i end }
 
-#dir_config('opencv', "/opt/local/include/opencv", "/opt/local/lib")
-#dir_config('qrencode', "/opt/local/include", "/opt/local/lib")
+dir_config('opencv'  , HEADER_DIRS, LIB_DIRS)
+dir_config('qrencode', HEADER_DIRS, LIB_DIRS)
 %w{ qrencode opencv_core opencv_highgui }.each do |lib|
   abort "need #{lib}" unless have_library(lib)
 end
