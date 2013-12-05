d568f0ff6edeab1361f67080a610936e92915777
George Thiruvathukal <george@thiruva.com>
2013-12-04 19:57:32 -0800

    Trying to fix location of libs and header directories

--- ext/qrtools/extconf.rb
+++ ext/qrtools/extconf.rb
@@ -9,10 +9,16 @@ HEADER_DIRS = [
     '/opt/local/include/opencv',
 ]
 
+HEADER_DIRS += `pkg-config --cflags opencv`.split.collect { |i| if i.start_with?("-I") then i[2..-1] else i end }
+HEADER_DIRS += `pkg-config --cflags libqrencode`.split.collect { |i| if i.start_with?("-I") then i[2..-1] else i end }
+
 LIB_DIRS = [
   LIBDIR,
   '/opt/local/lib',
 ]
+
+LIB_DIRS += `pkg-config --libs opencv`.split.collect { |i| if i.start_with?("-L") then i[2..-1] else i end }
+LIB_DIRS += `pkg-config --libs libqrencode`.split.collect { |i| if i.start_with?("-L") then i[2..-1] else i end }
 
 dir_config('opencv', "/opt/local/include/opencv", "/opt/local/lib")
 dir_config('qrencode', "/opt/local/include", "/opt/local/lib")
